df32441dfbef578b0a310a00151ef9133f5a9c36
swamy582 <dongalaanjaneyaswamy@gmail.com>
2025-06-05 19:17:48 +0530

    Update Signup.jsx

--- Jarrys/src/Components/Signup.jsx
+++ Jarrys/src/Components/Signup.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
-import "../styles/Signup.css";
+import "../Styles/Signup.css";
 
 const Signup = () => {
   const [name, setName] = useState("");

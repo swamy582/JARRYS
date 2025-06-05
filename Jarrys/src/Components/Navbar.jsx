b97b33f646f962b4de5dd796e4d5a49797bc661f
swamy582 <dongalaanjaneyaswamy@gmail.com>
2025-06-05 21:32:40 +0530

    Update Navbar.jsx

--- Jarrys/src/Components/Navbar.jsx
+++ Jarrys/src/Components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { FaSearch, FaShoppingCart, FaUser } from "react-icons/fa";
-import "../styles/Navbar.css";
+import "../Styles/Navbar.css";
 import logo from "../assets/logo.png"; // Ensure this image is in the assets folder
 
 const Navbar = () => {
